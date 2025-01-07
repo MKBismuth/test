4db21dff49619c5ba8043fdbd1ca70eb076f1420
Bismuth <89401638+MKBismuth@users.noreply.github.com>
2025-01-07 20:17:43 +0330

    Create index.php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport"
+    content="width=device-width" initial-scale="1.0">
+  <style>
+    
+  </style>
+  <link rel="icon" type="image/webp" href="bear.gif">
+  <title>T1</title>
+  <title>TtTitle</title>
+  <title>T3</title>
+</head>
+<body>
+  <?php echo "tt %@ fa fs"?>
+  <p>this is a paragraph</p>
+  <form target="_blank">
+    <input type="text" name="iint">
+    <input type="submit">
+  </form>
+  
+
+  <script>
+    function sss(){
+      const ss = new SpeechSynthesisUtterance("hello world");
+      window.speechSynthesis.speak(ss);
+    }
+  </script>
+  
+</body>
+</html>
